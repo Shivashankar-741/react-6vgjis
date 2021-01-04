129d673c2d2604aca004f740c147faadd6261aac
Shivashankar-741 <shiva1894300@gmail.com>
2021-01-04 19:44:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useReducer } from "react";
+import "./style.css";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+    </div>
+  );
+}
